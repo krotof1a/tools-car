d00f8374a7d33e0519f4224cb6bcfff981370eb1
krotof1a <christophe.safra@laposte.net>
2018-09-14 18:31:02 +0000

    Add network wait

--- scripts/updateChip.sh
+++ scripts/updateChip.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Wait for network availability
+cur=`ping -c 1 github.com > /dev/zero 2>&1 ; echo $?`
+while [ $cur -ne 0 ]
+do
+	echo "Wait for network !"
+	sleep 1
+	cur=`ping -c 1 github.com > /dev/zero 2>&1 ; echo $?`
+done
+
 # Update chip directory from GitHub
 cd /home/chip/tools-car
 git pull
