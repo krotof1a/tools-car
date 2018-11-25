750c72e8a7b5546faaf6c603a1a1f3aae4a06c43
krotof1a <christophe.safra@laposte.net>
2018-11-25 22:15:47 +0000

    Corrects shutdown path

--- scripts/lowBatShutdown.sh
+++ scripts/lowBatShutdown.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+sleep 120
+
 while true
 do
 	REGB9H=`i2cget -f -y 0 0x34 0xb9`  # Read AXP209 register B9H
 	PERC_CHG=$(($REGB9H))  # convert to decimal
 	#echo "$PERC_CHG"
-	if [ $PERC_CHG -le 90 ]
+	if [ $PERC_CHG -le 60 ]
 	then
 		echo "Stopping"
-		shutdown -h now
+		/sbin/shutdown -h now
 	fi
 	sleep 60
 done
